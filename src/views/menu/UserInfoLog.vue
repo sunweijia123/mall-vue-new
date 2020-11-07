4d8f17765b66e59f79ca86c2949e05bd2d8959fc
sunweijia <sunweijia1123@126.com>
2020-11-07 22:00:22 +0800

    修改title

--- src/views/menu/UserInfoLog.vue
+++ src/views/menu/UserInfoLog.vue
@@ -6,16 +6,17 @@
       <el-button id="newCustomer" type="primary" @click="newCustomer">新增客户</el-button>
     </div>
     <Table :tableData="tableData" @transdata="getRefresh(arguments)" @edit="getEdit(arguments)"></Table>
-    <el-pagination
+    <div id="foot">
+      <el-pagination
         background
         layout="prev, pager, next"
         :current-page="pageNum"
         :page-count="pageSize"
         @current-change="handleCurrentChange"
-        class="paginationShow"
-    ></el-pagination>
+      ></el-pagination>
+    </div>
     <!-- 新增弹框 -->
-    <el-dialog title="新洲" :visible.sync="dialogFormVisible">
+    <el-dialog title="新增客户信息" :visible.sync="dialogFormVisible">
       <el-form :model="form">
         <el-form-item label="姓名:" :label-width="formLabelWidth">
           <el-input v-model="form.name" autocomplete="off"></el-input>
@@ -160,12 +161,33 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.m- {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
 #head {
   width: 100%;
   height: 6%;
   text-align: right;
 }
+// #main {
+//   flex: 1;
+//   overflow-y: scroll;
+//   background: #858585;
+// }
+#foot {
+  width: 100%;
+  height: 6%;
+  background: #808080;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+}
 .el-form-item__label {
   text-align: center !important;
+}
+#newCustomer {
 }
 </style>
